dabf4a0648b9d12c7d6f81257f28b6e7783e385e
bilogic <946010+bilogic@users.noreply.github.com>
2024-10-31 18:52:52 +0800

    remove utc_seconds for now

--- components/Dcf77/switch.py
+++ components/Dcf77/switch.py
@@ -9,7 +9,7 @@ CONF_UTC = "utc_seconds"
 
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Dcf77),    
-    cv.Optional(CONF_UTC): 28800,
+    # cv.Optional(CONF_UTC): 28800,
     # cv.Required(CONF_UTC): cv.use_id(uart.UARTComponent)
 }).extend(cv.COMPONENT_SCHEMA)
 
@@ -17,4 +17,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield switch.register_switch(var, config)    
-    cg.add(var.set_utc(config[CONF_UTC]))
+    # cg.add(var.set_utc(config[CONF_UTC]))
