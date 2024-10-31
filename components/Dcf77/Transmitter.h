feae9b04ddda6e959e6c914498d6b8b7aa5d99f6
bilogic <946010+bilogic@users.noreply.github.com>
2024-11-01 00:46:44 +0800

    fix

--- components/Dcf77/Transmitter.h
+++ components/Dcf77/Transmitter.h
@@ -7,7 +7,7 @@ extern int actualHours, actualMinutes, actualSecond, actualDay, actualMonth, act
 extern int impulseArray[60];
 extern int impulseCount;
 extern int timeRunningContinuous;
-extern time::RealTimeClock* time_id;
+extern esphome::time::RealTimeClock* time_id;
 extern Ticker tickerDecisec; // TBD at 100ms
 
 int Bin2Bcd(int dato);
