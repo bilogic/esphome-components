d1c111e119ac02cac34f2cd95625dd92cc339000
bilogic <946010+bilogic@users.noreply.github.com>
2024-10-31 21:35:55 +0800

    fix

--- components/Dcf77/Dcf77.cpp
+++ components/Dcf77/Dcf77.cpp
@@ -11,7 +11,21 @@ void Dcf77::setup() {
     ESP_LOGCONFIG(TAG, "Dcf77 setup()");
 }
 
-void Dcf77::write_state(bool state) {    
+void Dcf77::write_state(bool state) {   
+     if (this->time_id_.has_value()) {
+        time::ESPTime now = this->time_id->now();
+        uint8_t year = now.year - 2000;
+        uint8_t month = now.month;
+        uint8_t day_of_month = now.day_of_month;
+        uint8_t hour = now.hour;
+        uint8_t minute = now.minute;
+        uint8_t second = now.second;
+        ESP_LOGW(TAG, "date %d-%d-%d", year, month, day_of_month);
+        ESP_LOGW(TAG, "date %d:%d:%d", hour, minute, second);
+      } else {
+        ESP_LOGW(TAG, "LOCAL_TIME_QUERY is not handled because time is not configured");
+      }
+
     ESP_LOGW(TAG, "W Dcf77 state()");
     ESP_LOGCONFIG(TAG, "Dcf77 setup()");
     this->publish_state(state);
--- components/Dcf77/Dcf77.h
+++ components/Dcf77/Dcf77.h
@@ -13,8 +13,10 @@ class Dcf77 : public switch_::Switch, public Component {
         void write_state(bool state) override;
         void dump_config() override;
         void set_utc(uint32_t utc_seconds) { this->utc_seconds = utc_seconds; };
-  
+        void set_time_id(time::RealTimeClock *time_id) { this->time_id_ = time_id; };
+
     protected:
+        optional<time::RealTimeClock *> time_id_{};
         uint32_t utc_seconds;
 };
 
