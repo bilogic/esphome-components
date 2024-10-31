6a50bbf6ee10b3f60d37ce34836e0cf368b0418a
bilogic <946010+bilogic@users.noreply.github.com>
2024-10-31 18:52:00 +0800

    changes

--- components/Dcf77/Dcf77.cpp
+++ components/Dcf77/Dcf77.cpp
@@ -7,8 +7,13 @@ namespace Bilogic {
 static const char *TAG = "Dcf77.switch";
 
 void Dcf77::setup() {
-
+    ESP_LOGCONFIG(TAG, "Dcf77 setup()");
 }
+
+void Dcf77::loop() {
+    ESP_LOGCONFIG(TAG, "Dcf77 loop()");
+}
+
 
 void Dcf77::write_state(bool state) {
 
